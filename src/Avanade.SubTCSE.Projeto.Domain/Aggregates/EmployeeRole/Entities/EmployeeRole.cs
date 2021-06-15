d6d11771678fdccdbd60b0706ff30472baf4e18e
Erick Oliveira <usrerick@gmail.com>
2021-06-15 09:00:01 -0300

    Estrutura do projeto/Inicio das regras de dominio

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,17 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; init; }
+
+        public string RoleName { get; init; }
+
+
+    }
+}
