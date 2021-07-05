0897815a65ca8de2b40e94507f66f1b34a4d5b8b
Erick Oliveira <usrerick@gmail.com>
2021-07-05 08:46:55 -0300

    Inclusao do context do mongodb

--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/BaseEntity.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/BaseEntity.cs
@@ -1,5 +1,9 @@
 ﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates
 {
+    /// <summary>
+    /// Classe base para definição do id das classes
+    /// </summary>
+    /// <typeparam name="Tid">Parametro alias(apelido) para o tipo de dado do id da classe origem</typeparam>
     public record BaseEntity<Tid>
     {
         public Tid Id { get; set; }
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Base/Repository/MongoDB/IMongoDBContext.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Driver;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Base.Repository.MongoDB
+{
+    public interface IMongoDBContext
+    {
+        IMongoCollection<TEntity> GetCollection<TEntity>(string collection);
+    }
+}
